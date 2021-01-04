ce65cb1e56b7276aef6959e7c8639c2e79ca7044
Scott Mathieson <scott@eingress.io>
2021-01-04 01:22:37 +0000

    Enable fastify option passthrough

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import api from './api';
 
 const { exit, stderr, stdout } = process;
 
+const API_OPTS = {
+  // See https://www.fastify.io/docs/latest/Server
+};
+
 const run = async () => {
   try {
-    await api.run();
+    await api.run(API_OPTS);
   } catch (error) {
     stderr.write(`${error}\n`);
     exit(1);
