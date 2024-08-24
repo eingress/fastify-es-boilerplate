f8ba9e24bfd3b403625ae0d88dd47759b7279e82
Scott Mathieson <scott@eingress.io>
2024-08-24 12:49:38 +0100

    Concatenate base64 string for formatting

--- src/plugins/favicon.js
+++ src/plugins/favicon.js
@@ -2,10 +2,12 @@ import fp from 'fastify-plugin';
 
 // Returns a transparent favicon.ico
 
-const ico = Buffer.from(
-  'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII',
-  'base64',
-).toString('binary');
+const ico64 =
+  'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T/////' +
+  '//8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSM' +
+  'glEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII';
+
+const ico = Buffer.from(ico64, 'base64').toString('binary');
 
 export default fp(async (fastify) => {
   fastify.get('/favicon.ico', (_, reply) => {
